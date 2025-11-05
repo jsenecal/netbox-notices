b35010771d82ba5cd33836b7f04043cd6677871a
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2025-11-05 01:56:55 +0000

    fix: remove deprecated Calendar widget import
    Comment out MaintenanceCalendarWidget that depended on removed
    Calendar class. Widget will be reimplemented with FullCalendar.js
    in a future update.

--- vendor_notification/widgets.py
+++ vendor_notification/widgets.py
@@ -7,7 +7,6 @@ from extras.dashboard.utils import register_widget
 from extras.dashboard.widgets import DashboardWidget
 
 from .models import Maintenance
-from .views import Calendar
 
 
 @register_widget
@@ -35,20 +34,25 @@ class UpcomingMaintenanceWidget(DashboardWidget):
         )
 
 
-@register_widget
-class MaintenanceCalendarWidget(DashboardWidget):
-    default_title = "Upcoming Circuit Maintenance Calendar"
-    description = "Show a simplified calendar view showing upcoming maintenance events this month"
-    template_name = "vendor_notification/calendar_widget.html"
-    width = 8
-    height = 8
+# MaintenanceCalendarWidget removed - replaced by full calendar view
+# The dashboard widget used the old HTMLCalendar implementation
+# which was replaced by FullCalendar.js in the main calendar view.
+# Dashboard calendar widget will be reimplemented in a future update.
 
-    def render(self, request):
-        curr_month = datetime.date.today()
-
-        # Load calendar
-        cal = Calendar()
-        html_calendar = cal.formatmonth(curr_month.year, curr_month.month)
-        html_calendar = html_calendar.replace("<td ", '<td  width="100" height="100"')
-
-        return render_to_string(self.template_name, {"calendar": mark_safe(html_calendar)})
+# @register_widget
+# class MaintenanceCalendarWidget(DashboardWidget):
+#     default_title = "Upcoming Circuit Maintenance Calendar"
+#     description = "Show a simplified calendar view showing upcoming maintenance events this month"
+#     template_name = "vendor_notification/calendar_widget.html"
+#     width = 8
+#     height = 8
+#
+#     def render(self, request):
+#         curr_month = datetime.date.today()
+#
+#         # Load calendar
+#         cal = Calendar()
+#         html_calendar = cal.formatmonth(curr_month.year, curr_month.month)
+#         html_calendar = html_calendar.replace("<td ", '<td  width="100" height="100"')
+#
+#         return render_to_string(self.template_name, {"calendar": mark_safe(html_calendar)})
