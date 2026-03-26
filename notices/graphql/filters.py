feaed19e7c3effe9347676013b267a18887d31f6
Elliot Anderson <elliot.anderson@indexexchange.com>
2026-03-26 17:59:12 +0000

    fix: `DuplicatedTypeName` exception thrown in v4.5.5

--- notices/graphql/filters.py
+++ notices/graphql/filters.py
@@ -1,7 +1,7 @@
 import strawberry_django
 from netbox.graphql.filters import PrimaryModelFilter
 from strawberry.scalars import ID
-from strawberry_django import FilterLookup
+from strawberry_django import StrFilterLookup
 
 from notices import models
 
@@ -17,43 +17,43 @@ __all__ = (
 
 @strawberry_django.filter_type(models.Maintenance, lookups=True)
 class MaintenanceFilter(PrimaryModelFilter):
-    name: FilterLookup[str] | None = strawberry_django.filter_field()
-    summary: FilterLookup[str] | None = strawberry_django.filter_field()
-    status: FilterLookup[str] | None = strawberry_django.filter_field()
+    name: StrFilterLookup | None = strawberry_django.filter_field()
+    summary: StrFilterLookup | None = strawberry_django.filter_field()
+    status: StrFilterLookup | None = strawberry_django.filter_field()
     provider_id: ID | None = strawberry_django.filter_field()
     acknowledged: bool | None = strawberry_django.filter_field()
 
 
 @strawberry_django.filter_type(models.Outage, lookups=True)
 class OutageFilter(PrimaryModelFilter):
-    name: FilterLookup[str] | None = strawberry_django.filter_field()
-    summary: FilterLookup[str] | None = strawberry_django.filter_field()
-    status: FilterLookup[str] | None = strawberry_django.filter_field()
+    name: StrFilterLookup | None = strawberry_django.filter_field()
+    summary: StrFilterLookup | None = strawberry_django.filter_field()
+    status: StrFilterLookup | None = strawberry_django.filter_field()
     provider_id: ID | None = strawberry_django.filter_field()
     acknowledged: bool | None = strawberry_django.filter_field()
 
 
 @strawberry_django.filter_type(models.Impact, lookups=True)
 class ImpactFilter(PrimaryModelFilter):
-    impact: FilterLookup[str] | None = strawberry_django.filter_field()
+    impact: StrFilterLookup | None = strawberry_django.filter_field()
     event_object_id: ID | None = strawberry_django.filter_field()
     target_object_id: ID | None = strawberry_django.filter_field()
 
 
 @strawberry_django.filter_type(models.EventNotification, lookups=True)
 class EventNotificationFilter(PrimaryModelFilter):
-    subject: FilterLookup[str] | None = strawberry_django.filter_field()
-    email_from: FilterLookup[str] | None = strawberry_django.filter_field()
+    subject: StrFilterLookup | None = strawberry_django.filter_field()
+    email_from: StrFilterLookup | None = strawberry_django.filter_field()
 
 
 @strawberry_django.filter_type(models.NotificationTemplate, lookups=True)
 class NotificationTemplateFilter(PrimaryModelFilter):
-    name: FilterLookup[str] | None = strawberry_django.filter_field()
-    slug: FilterLookup[str] | None = strawberry_django.filter_field()
-    event_type: FilterLookup[str] | None = strawberry_django.filter_field()
+    name: StrFilterLookup | None = strawberry_django.filter_field()
+    slug: StrFilterLookup | None = strawberry_django.filter_field()
+    event_type: StrFilterLookup | None = strawberry_django.filter_field()
 
 
 @strawberry_django.filter_type(models.PreparedNotification, lookups=True)
 class PreparedNotificationFilter(PrimaryModelFilter):
-    status: FilterLookup[str] | None = strawberry_django.filter_field()
-    subject: FilterLookup[str] | None = strawberry_django.filter_field()
+    status: StrFilterLookup | None = strawberry_django.filter_field()
+    subject: StrFilterLookup | None = strawberry_django.filter_field()
