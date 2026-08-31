b0c9c835dc4f9c1048f8b0400dcbfb7c10b7d4b7
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2026-08-31 07:11:29 +0000

    chore: bump version 1.1.2 -> 1.2.0

--- notices/__init__.py
+++ notices/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 __author__ = """Jonathan Senecal"""
 __email__ = "contact@jonathansenecal.com"
-__version__ = "1.1.2"
+__version__ = "1.2.0"
 
 
 from netbox.plugins import PluginConfig
